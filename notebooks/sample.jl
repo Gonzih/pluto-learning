d090abd69fac7b51980a2ce58b5a38aa47a8c5ac
Max Gonzih <gonzih@gmail.com>
2022-06-25 20:24:35 -0700

    Ok, this is first step

--- notebooks/sample.jl
+++ notebooks/sample.jl
@@ -5,7 +5,7 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 81242c9c-f3d7-11ec-0795-7f3092649d9f
-
+data_url = "https://github.com/cryptoratsdev/senate-disclosures/blob/main/2022FD.json"
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
