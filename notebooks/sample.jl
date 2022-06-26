4c6d923ee5ea48a58199a1a7b9581ba0083912b5
Max Gonzih <gonzih@gmail.com>
2022-06-25 20:16:38 -0700

    Huh

--- notebooks/sample.jl
+++ notebooks/sample.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.9
+# v0.19.9
 
 using Markdown
 using InteractiveUtils
@@ -7,5 +7,22 @@ using InteractiveUtils
 # ╔═╡ 81242c9c-f3d7-11ec-0795-7f3092649d9f
 
 
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.3"
+manifest_format = "2.0"
+
+[deps]
+"""
+
 # ╔═╡ Cell order:
 # ╠═81242c9c-f3d7-11ec-0795-7f3092649d9f
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
